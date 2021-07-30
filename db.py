2ed0987c963955118fa2a59d5a27c0418135c035
NGWORKS <mikulab@163.com>
2021-07-30 22:26:18 +0800

    数据库的定义和基本操作

--- db.py
+++ db.py
@@ -1,3 +1,10 @@
+"""
+此文件是用于连接和初始化数据库的程序文件。
+
+>> 用于建立一个数据库连接
+>> 新建数据库文件、新建数据表
+>> 封装了一个数据库操作的类。
+"""
 from taskslist import UPLOAD
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,7 +18,7 @@ engine = create_engine('sqlite:///Recorder.db', encoding='utf-8', echo=False)
 DbSession = sessionmaker(bind=engine)
 Base = declarative_base()
 
-# 表信息
+# 这个是进度数据表，里面定于了每个任务唯一的ID和任务进行程度，同时记录一些必要的数据。
 class Rate(Base):
     __tablename__ = "Rate"
     TaskId = Column(Text, nullable=False, primary_key=True)
@@ -30,7 +37,7 @@ class Rate(Base):
     Transcode = Column(Boolean,default=None)
     Upload = Column(Boolean,default=None)
 
-
+# 这个用来记录 录播姬 传来的原始数据，以供分析。
 class Recorder(Base):
     __tablename__ = "Recorder"
     EventId = Column(String(36), nullable=False, primary_key=True)
@@ -66,6 +73,7 @@ Base.metadata.create_all(engine)
 
 class RecorderDB:
     def __init__(self,Table):
+        # 新建连接
         self.session = DbSession()
         self.Table = Table
 
@@ -73,11 +81,17 @@ class RecorderDB:
         return self
 
     def add(self,**dt):
+        """
+        这是向数据表中添加一条记录的方法
+        """
         new_user = self.Table(**dt)
         # 添加到session
         self.session.add(new_user)
     
     def filter(self,**dt):
+        """
+        这是使用特定条件进行查询，同时使用返回的结果在关闭前可以进行数据更新。
+        """
         Data = self.session.query(self.Table).filter_by(**dt).all()
         return Data
 
@@ -88,11 +102,10 @@ class RecorderDB:
             print(f'exc_value: {exc_value}')
             print(f'exc_traceback: {exc_tb}')
             print('exception handled')
+            # 如果出现错误，回滚。
             self.session.rollback()
+        # 提交更改
         self.session.commit()
+        # 关闭连接
         self.session.close()
-        return True # 异常处理后必须返回True
-
-
-# user = session.query(QQ).filter_by(qq=10001).all()
-# print(user)
+        return True 
